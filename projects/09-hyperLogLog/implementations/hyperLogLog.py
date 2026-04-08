bbc180132ff4806344b8da4d71517f82e7175d41
Andy Liang <andyliang810@gmail.com>
2026-04-08 00:02:19 -0400

    improve readability

--- projects/09-hyperLogLog/implementations/hyperLogLog.py
+++ projects/09-hyperLogLog/implementations/hyperLogLog.py
@@ -10,53 +10,50 @@ import sys
 class HyperLogLog:
     def __init__(self, p):
         self.p = p
-        self.m = 1 << p
-        self.reg = [0] * self.m
+        self.num_registers = 1 << p
+        self.reg = [0] * self.num_registers
 
-    def add(self, value):
-        x = int.from_bytes(hashlib.sha1(str(value).encode()).digest()[:8], "big")
-        j = x >> (64 - self.p)
-        wbits = 64 - self.p
-        w = x & ((1 << wbits) - 1)
+    def add_element(self, value):
+        hash = int.from_bytes(hashlib.sha1(str(value).encode()).digest()[:8], "big")
+        register_index = hash >> (64 - self.p)
+        remaining_bits = 64 - self.p
+        w = hash & ((1 << remaining_bits) - 1)
         if w == 0:
-            r = wbits + 1
+            num_leading_zeros = remaining_bits + 1
         else:
-            r = (wbits - w.bit_length()) + 1
-        if r > self.reg[j]:
-            self.reg[j] = r
+            num_leading_zeros = (remaining_bits - w.bit_length()) + 1
+        if num_leading_zeros > self.reg[register_index]:
+            self.reg[register_index] = num_leading_zeros
 
-    def estimate(self):
-        m = self.m
-        if m == 16:
-            a = 0.673
-        elif m == 32:
-            a = 0.697
-        elif m == 64:
-            a = 0.709
+    def estimate_cardinality(self):
+        num_registers = self.num_registers
+        if num_registers == 16: # bias correction from hyper log log paper
+            bias_correction = 0.673
+        elif num_registers == 32:
+            bias_correction = 0.697
+        elif num_registers == 64:
+            bias_correction = 0.709
         else:
-            a = 0.7213 / (1 + 1.079 / m)
+            bias_correction = 0.7213 / (1 + 1.079 / num_registers)
 
-        z = sum(2.0 ** (-r) for r in self.reg)
-        e = a * m * m / z
+        harmonic_sum = sum(2.0 ** (-r) for r in self.reg)
+        cardinality_estimate = bias_correction * num_registers * num_registers / harmonic_sum
 
-        v = self.reg.count(0)
-        if e <= 2.5 * m and v > 0:
-            return m * math.log(m / v)
+        num_registers_empty = self.reg.count(0)
+        if cardinality_estimate <= 2.5 * num_registers and num_registers_empty > 0:
+            return num_registers * math.log(num_registers / num_registers_empty)
 
         two64 = float(1 << 64)
-        if e > two64 / 30.0:
-            return -two64 * math.log(1.0 - e / two64)
+        if cardinality_estimate > two64 / 30.0:
+            return -two64 * math.log(1.0 - cardinality_estimate / two64)
 
-        return e
+        return cardinality_estimate
 
-
-def main():
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         t = open(sys.argv[1], "r", encoding="utf-8").read().split()
     else:
         t = sys.stdin.read().split()
-    if not t:
-        return
 
     i = 0
     tc = int(t[i])
@@ -70,13 +67,9 @@ def main():
 
         h = HyperLogLog(p)
         for _ in range(n):
-            h.add(t[i])
+            h.add_element(t[i])
             i += 1
 
-        out.append(str(int(round(h.estimate()))))
+        out.append(str(int(round(h.estimate_cardinality()))))
 
     sys.stdout.write("\n".join(out))
-
-
-if __name__ == "__main__":
-    main()
