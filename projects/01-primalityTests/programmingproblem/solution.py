0615faa95041fc77b08a0cd6e23ce62b4e668ffd
elsanorm17 <elsanorm17@gmail.com>
2026-04-19 14:09:52 -0400

    update solution.py to use bidirectional search

--- projects/01-primalityTests/programmingproblem/solution.py
+++ projects/01-primalityTests/programmingproblem/solution.py
@@ -45,7 +45,7 @@ def is_prime_miller_rabin(n, k=5):
         d //= 2
         r += 1
 
-    for a in {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}:
+    for a in {2, 325, 9375, 28178, 450775, 9780504, 1795265022}:
         if (n == a):
             return True
         if (check_composite(n, a, d, r)):
@@ -53,84 +53,136 @@ def is_prime_miller_rabin(n, k=5):
     
     return True
 
-def rec(prev, src, target):
-    if (src in visited):
+
+def get_neighbors(num):
+    neighbors = []
+    s = list(str(num))
+
+    for i in range(len(s)):
+        original = s[i]
+        for d in '0123456789':
+            if d == original:
+                continue
+
+            s[i] = d
+            new_num = int("".join(s))
+
+            # avoid leading zero
+            if s[0] != '0':
+                neighbors.append(new_num)
+
+        s[i] = original
+
+    return neighbors
+
+
+def prime_path(src, target):
+    queue = deque([src])
+    visited = set([src])
+    parent = {src: -1}
+
+    while queue:
+        cur = queue.popleft()
+
+        if cur == target:
+            # reconstruct path
+            path = []
+            while cur != -1:
+                path.append(cur)
+                cur = parent[cur]
+            return path[::-1]
+
+        for nei in get_neighbors(cur):
+            if nei % 2 == 0 or nei % 5 == 0 or nei % 3 == 0:
+                continue
+            if nei not in visited and is_prime_miller_rabin(nei):
+                visited.add(nei)
+                parent[nei] = cur
+                queue.append(nei)
+
+    return None
+
+def reconstruct(meet, parent1, parent2):
+    path1 = []
+    cur = meet
+    while cur != -1:
+        path1.append(cur)
+        cur = parent1[cur]
+    path1.reverse()
+
+    path2 = []
+    cur = parent2[meet]
+    while cur != -1:
+        path2.append(cur)
+        cur = parent2[cur]
+
+    return path1 + path2
+
+
+def bidirectional_prime_path(src, target):
+    if src == target:
+        return [src]
+
+    # frontiers
+    front1 = {src}
+    front2 = {target}
+
+    # visited
+    visited1 = {src}
+    visited2 = {target}
+
+    # parents
+    parent1 = {src: -1}
+    parent2 = {target: -1}
+
+    while front1 and front2:
+        # expand smaller frontier
+        if len(front1) > len(front2):
+            front1, front2 = front2, front1
+            visited1, visited2 = visited2, visited1
+            parent1, parent2 = parent2, parent1
+
+        next_front = set()
+
+        for node in front1:
+            for nei in get_neighbors(node):
+                if nei in visited1:
+                    continue
+
+                if not is_prime_miller_rabin(nei):
+                    continue
+
+                parent1[nei] = node
+
+                if nei in visited2:
+                    return reconstruct(nei, parent1, parent2)
+
+                visited1.add(nei)
+                next_front.add(nei)
+
+        front1 = next_front
+
+    return None
+
+
+def main():
+    src, target = map(int, input().split())
+
+    if not is_prime_miller_rabin(src) or not is_prime_miller_rabin(target):
+        print("impossible")
         return
-    
-    visited.add(src)
 
-    if (not is_prime_miller_rabin(src) and src != target and prev != -1):
-        return
-    
-    if not prev in outEdges:
-        outEdges[prev] = list()
-    outEdges[prev].append(src)
+    path = bidirectional_prime_path(src, target)
 
-    if (src == target):
-        return
-    
-    upperPart = src
-    lowerPart = 0
-    chunk = src
-    mult = 1
+    if path is None:
+        print("impossible")
+    else:
+        if (path[0] == src):
+            for x in path:
+                print(x)
+        else:
+            for x in reversed(path):
+                print(x)
 
-    while (upperPart > 0):
-        rem = chunk % 10
-        upperPart -= rem * mult
-
-        upOne = (rem + 1) % 10
-        downOne = rem - 1 if rem - 1 >= 0 else 9
-        upperRec = upperPart + mult * upOne + lowerPart
-        downerRec = upperPart + mult * downOne + lowerPart
-        rec(src, upperRec, target)
-        rec(src, downerRec, target)
-
-        lowerPart += rem * mult
-        chunk //= 10
-        mult *= 10
-
-vals = input().split(' ')
-src = int(vals[0])
-target = int(vals[1])
-
-outEdges = dict()
-visited = set()
-
-rec(-1, src, target)
-
-
-if not outEdges:
-    print("impossible")
-    exit()
-
-seen = set()
-
-parent = dict()
-total = len(visited)
-current = 0
-
-bfs = deque()
-bfs.append(-1)
-while (current < total and bfs):
-    curVal = bfs.popleft()
-    if not curVal in outEdges:
-        continue
-    for dest in outEdges[curVal]:
-        if dest == target:
-            visitedList = list()
-            visitedList.append(dest)
-            while (parent[curVal] != -1):
-                visitedList.append(curVal)
-                curVal = parent[curVal]
-            visitedList.append(src)
-            for item in reversed(visitedList):
-                print(item)
-            exit()
-            
-        if not dest in seen:
-            parent[dest] = curVal
-            bfs.append(dest)
-            seen.add(dest)
-            current += 1
-
-print("impossible")
+if __name__ == "__main__":
+    main()
